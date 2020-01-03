172215e2ba5feb1c294cb26e1c4f75152562e169
Глеб Брыкин <glebbrykin@yandex.ru>
2020-01-03 18:10:32 +0300

    Add files via upload

---
+++ Implementation/src/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿//***************************************************************************************************
+//* (C) ColorfulSoft corp., 2019. All rights reserved.
+//* The code is available under the Apache-2.0 license. Read the License for details.
+//***************************************************************************************************
+
+//-> Latest commit: Brykin Gleb, 02.01.2020.
+
+using System;
+using System.Reflection;
+
+    [assembly: AssemblyTitle("Torch.NET")]
+    [assembly: AssemblyDescription("Neural network environment for MS .NET Framework.")]
+    [assembly: AssemblyConfiguration("")]
+    [assembly: AssemblyVersion("0.02.01.2020")]
+    [assembly: AssemblyCompany("ColorfulSoft")]
+    [assembly: AssemblyProduct("System.AI")]
+    [assembly: AssemblyCopyright("© ColorfulSoft")]
+    [assembly: AssemblyTrademark("The System.AI project")]
+    [assembly: AssemblyCulture("")]
