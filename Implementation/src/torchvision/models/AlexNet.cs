664ea77aab03a430d237e921d05d82336bbbd720
Глеб Брыкин <glebbrykin@yandex.ru>
2020-01-03 18:24:33 +0300

    Add files via upload

---
+++ Implementation/src/torchvision/models/AlexNet.cs
@@ -0,0 +1,34 @@
+﻿//***************************************************************************************************
+//* (C) ColorfulSoft corp., 2019. All rights reserved.
+//* The code is available under the Apache-2.0 license. Read the License for details.
+//***************************************************************************************************
+
+//-> Latest commit: Brykin Gleb, 01.01.2020.
+
+using System;
+
+namespace System
+{
+
+    namespace AI
+    {
+
+        using nn = torch.nn;
+
+        public static partial class torchvision
+        {
+
+            public static partial class models
+            {
+
+                public sealed class AlexNet : nn.Module
+                {
+                }
+
+            }
+
+        }
+
+    }
+
+}
