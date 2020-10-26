d56f6a2bafc638b29029a82d4f369dbb7629b47d
Глеб Брыкин <glebbrykin@yandex.ru>
2020-10-26 14:29:45 +0300

    Add files via upload

---
+++ Implementation/src/DotnetExtension/StreamExtension.cs
@@ -0,0 +1,26 @@
+﻿//***************************************************************************************************
+//* (C) ColorfulSoft corp., 2020. All rights reserved.
+//* The code is available under the Apache-2.0 license. Read the License for details.
+//***************************************************************************************************
+
+using System;
+
+namespace System
+{
+
+    namespace IO
+    {
+
+        public static class StreamExtension
+        {
+
+            public static void Write(this Stream s, byte[] data)
+            {
+                s.Write(data, 0, data.Length);
+            }
+
+        }
+
+    }
+
+}
