407d370b3ffa9171bcce974db005420fcad0a92a
Глеб Брыкин <glebbrykin@yandex.ru>
2020-10-26 14:27:09 +0300

    Add files via upload

---
+++ Implementation/src/imageio/StbImageWriteSharp/ColorComponents.cs
@@ -0,0 +1,32 @@
+﻿namespace System
+{
+
+    namespace AI
+    {
+
+        public static partial class imageio
+        {
+
+            internal static partial class StbImageWriteSharp
+            {
+
+                public enum ColorComponents
+                {
+
+                    Grey = 1,
+
+                    GreyAlpha = 2,
+
+                    RedGreenBlue = 3,
+
+                    RedGreenBlueAlpha = 4
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
