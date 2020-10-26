5e4370f8b2cf7cc95ead1bd90dc65aebe73387e1
Глеб Брыкин <glebbrykin@yandex.ru>
2020-10-26 14:28:41 +0300

    Add files via upload

---
+++ Implementation/src/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿//***************************************************************************************************
+//* (C) ColorfulSoft corp., 2019. All rights reserved.
+//* The code is available under the Apache-2.0 license. Read the License for details.
+//***************************************************************************************************
+
+using System.Reflection;
+
+    //-> Assembly information
+  
+    [assembly: AssemblyTitle("System.AI")]
+    [assembly: AssemblyDescription("Artificial Intelligence and machine learning library for .NET")]
+    [assembly: AssemblyConfiguration("")]
+    [assembly: AssemblyVersion("0.06.09.2020")]
+    [assembly: AssemblyCompany("ColorfulSoft corp.")]
+    [assembly: AssemblyProduct("System.AI")]
+    [assembly: AssemblyCopyright("© ColorfulSoft corp. and other")]
+    [assembly: AssemblyTrademark("System.AI")]
+    [assembly: AssemblyCulture("")]
