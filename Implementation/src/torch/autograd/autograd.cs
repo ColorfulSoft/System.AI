cf1dcd385cf7b3c9fd706a54d235005e1616371f
Глеб Брыкин <glebbrykin@yandex.ru>
2021-02-06 23:34:02 +0300

    Add files via upload

---
+++ Implementation/src/torch/autograd/autograd.cs
@@ -0,0 +1,25 @@
+﻿//***************************************************************************************************
+//* (C) ColorfulSoft corp., 2020. All rights reserved.
+//* The code is available under the Apache-2.0 license. Read the License for details.
+//***************************************************************************************************
+
+using System;
+
+namespace System
+{
+
+    namespace AI
+    {
+
+        public static partial class torch
+        {
+
+            public static partial class autograd
+            {
+            }
+
+        }
+
+    }
+
+}
