77ed2b6bce218882072bf53c2c1a45a7ad5ad72a
Alvin Seville <alvinseville7cf@gmail.com>
2021-03-23 21:11:50 +1000

    Compile.bat clean-up

--- Implementation/Compile.bat
+++ Implementation/Compile.bat
@@ -1,3 +1,4 @@
-MD "Release"
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe @"System.AI.rsp"
-cmd.exe
+set "csc_path=C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe"
+
+md "Release"
+"%csc_path%" @"System.AI.rsp"
