538abf3dd173158549284b8639ce40527dee3dee
Gleb S. Brykin <GlebSBrykin@yandex.ru>
2021-12-12 16:47:15 +0300

    Update and rename Implementation/src/Compile.bat to Implementation/Compile.bat

---
+++ Implementation/Compile.bat
@@ -0,0 +1,3 @@
+md "Release"
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe /unsafe /optimize /target:library /out:"Release\System.FloatingPoint.dll" /doc:"Release\System.FloatingPoint.xml" @"src/System.FloatingPoint/System.FloatingPoint.Windows.rsp"
+cmd.exe
--- Implementation/src/Compile.bat
+++
@@ -1,3 +0,0 @@
-md "Release"
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe /unsafe /optimize /target:library /out:"Release\System.FloatingPoint.dll" /doc:"Release\System.AI.xml" @"src/System.FloatingPoint/System.FloatingPoint.Windows.rsp"
-cmd.exe
