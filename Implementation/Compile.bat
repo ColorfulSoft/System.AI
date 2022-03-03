d61c9d51e76dfb52a84a5bd3388ea01c9f24f84e
Gleb S. Brykin <GlebSBrykin@yandex.ru>
2022-03-03 21:48:56 +0300

    Add files via upload

--- Implementation/Compile.bat
+++ Implementation/Compile.bat
@@ -1,3 +1,7 @@
 md "Release"
+
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe @"Configuration.rsp" /out:"Release\System.FloatingPoint.dll" /doc:"Release\System.FloatingPoint.xml" @"src/System.FloatingPoint/System.FloatingPoint.Windows.rsp"
+
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe @"Configuration.rsp" /r:"Release\System.FloatingPoint.dll" /out:"Release\imageio.dll" /doc:"Release\imageio.xml" @"src/imageio.NET/imageio.NET.Windows.rsp"
+
 cmd.exe
