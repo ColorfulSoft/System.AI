4f1c6993aaf491fd111ed2687d4d3ee3655df49c
Gleb S. Brykin <GlebSBrykin@yandex.ru>
2022-03-05 10:53:55 +0300

    Add files via upload

---
+++ Implementation/CompileSingle.bat
@@ -0,0 +1,6 @@
+md bin
+md bin\Release
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe @ConfigurationRel.rsp /out:bin\Release\System.AI.dll /doc:bin\Release\System.AI.xml @src/System.FloatingPoint/System.FloatingPoint.Windows.rsp @src/imageio.NET/imageio.NET.Windows.rsp
+md bin\Debug
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe @ConfigurationDeb.rsp /out:bin\Debug\System.AI.dll /doc:bin\Debug\System.AI.xml @src/System.FloatingPoint/System.FloatingPoint.Windows.rsp @src/imageio.NET/imageio.NET.Windows.rsp
+cmd.exe
