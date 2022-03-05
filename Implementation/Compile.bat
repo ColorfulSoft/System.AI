7d84ad79426863784e72afaf7564c1caf10c65dc
Gleb S. Brykin <GlebSBrykin@yandex.ru>
2022-03-05 10:08:27 +0300

    Add files via upload

--- Implementation/Compile.bat
+++ Implementation/Compile.bat
@@ -1,7 +1,14 @@
-md "Release"
+md "bin"
+md "bin\Release"
 
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe @"Configuration.rsp" /out:"Release\System.FloatingPoint.dll" /doc:"Release\System.FloatingPoint.xml" @"src/System.FloatingPoint/System.FloatingPoint.Windows.rsp"
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe @"ConfigurationRel.rsp" /out:"bin\Release\System.FloatingPoint.dll" /doc:"bin\Release\System.FloatingPoint.xml" @"src/System.FloatingPoint/System.FloatingPoint.Windows.rsp"
 
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe @"Configuration.rsp" /r:"Release\System.FloatingPoint.dll" /out:"Release\imageio.dll" /doc:"Release\imageio.xml" @"src/imageio.NET/imageio.NET.Windows.rsp"
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe @"ConfigurationRel.rsp" /r:"bin\Release\System.FloatingPoint.dll" /out:"bin\Release\imageio.dll" /doc:"bin\Release\imageio.xml" @"src/imageio.NET/imageio.NET.Windows.rsp"
+
+md "bin\Debug"
+
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe @"ConfigurationDeb.rsp" /out:"bin\Debug\System.FloatingPoint.dll" /doc:"bin\Debug\System.FloatingPoint.xml" @"src/System.FloatingPoint/System.FloatingPoint.Windows.rsp"
+
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe @"ConfigurationDeb.rsp" /r:"bin\Debug\System.FloatingPoint.dll" /out:"bin\Debug\imageio.dll" /doc:"bin\Debug\imageio.xml" @"src/imageio.NET/imageio.NET.Windows.rsp"
 
 cmd.exe
