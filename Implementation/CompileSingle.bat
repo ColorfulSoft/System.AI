91682b4f4ba26f52c7e2934281873a1435dc3656
Gleb S. Brykin <GlebSBrykin@yandex.ru>
2022-04-10 18:31:00 +0300

    Add files via upload

--- Implementation/CompileSingle.bat
+++ Implementation/CompileSingle.bat
@@ -1,6 +1,6 @@
 md bin
 md bin\Release
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe @ConfigurationRel.rsp /out:bin\Release\System.AI.dll /doc:bin\Release\System.AI.xml @src/System.FloatingPoint/System.FloatingPoint.Windows.rsp @src/imageio.NET/imageio.NET.Windows.rsp @src/warnings.NET/warnings.NET.Windows.rsp @src/System.Complex/System.Complex.Windows.rsp
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe @ConfigurationRel.rsp /out:bin\Release\System.AI.dll /doc:bin\Release\System.AI.xml @src/System.FloatingPoint/System.FloatingPoint.Windows.rsp @src/imageio.NET/imageio.NET.Windows.rsp @src/warnings.NET/warnings.NET.Windows.rsp @src/System.Complex/System.Complex.Windows.rsp @src/PyType.NET/PyType.NET.Windows.rsp
 md bin\Debug
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe @ConfigurationDeb.rsp /out:bin\Debug\System.AI.dll /doc:bin\Debug\System.AI.xml @src/System.FloatingPoint/System.FloatingPoint.Windows.rsp @src/imageio.NET/imageio.NET.Windows.rsp @src/warnings.NET/warnings.NET.Windows.rsp @src/System.Complex/System.Complex.Windows.rsp
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe @ConfigurationDeb.rsp /out:bin\Debug\System.AI.dll /doc:bin\Debug\System.AI.xml @src/System.FloatingPoint/System.FloatingPoint.Windows.rsp @src/imageio.NET/imageio.NET.Windows.rsp @src/warnings.NET/warnings.NET.Windows.rsp @src/System.Complex/System.Complex.Windows.rsp @src/PyType.NET/PyType.NET.Windows.rsp
 cmd.exe
