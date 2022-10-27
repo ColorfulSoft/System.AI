606a382fff9629afaba3dbab1f61bb1cba9ef16f
Gleb S. Brykin <GlebSBrykin@yandex.ru>
2022-10-27 09:31:57 +0300

    Add files via upload

--- Implementation/Compile.bat
+++ Implementation/Compile.bat
@@ -5,10 +5,12 @@ C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe @ConfigurationRel.rsp /out
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe @ConfigurationRel.rsp /r:bin\Release\System.FloatingPoint.dll /r:bin\Release\warnings.dll /out:bin\Release\imageio.dll /doc:bin\Release\imageio.xml @src/imageio.NET/imageio.NET.Windows.rsp
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe @ConfigurationRel.rsp /r:bin\Release\System.FloatingPoint.dll /out:bin\Release\System.Complex.dll /doc:bin\Release\System.Complex.xml @src/System.Complex/System.Complex.Windows.rsp
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe @ConfigurationRel.rsp /out:bin\Release\PyType.dll /doc:bin\Release\PyType.xml @src/PyType.NET/PyType.NET.Windows.rsp
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe @ConfigurationRel.rsp /r:bin\Release\System.FloatingPoint.dll /r:bin\Release\warnings.dll /out:bin\Release\torchfile.dll /doc:bin\Release\torchfile.xml @src/torchfile.NET/torchfile.NET.Windows.rsp
 md bin\Debug
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe @ConfigurationDeb.rsp /out:bin\Debug\System.FloatingPoint.dll /doc:bin\Debug\System.FloatingPoint.xml @src/System.FloatingPoint/System.FloatingPoint.Windows.rsp
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe @ConfigurationDeb.rsp /out:bin\Debug\warnings.dll /doc:bin\Debug\warnings.xml @src/warnings.NET/warnings.NET.Windows.rsp
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe @ConfigurationDeb.rsp /r:bin\Debug\System.FloatingPoint.dll /r:bin\Debug\warnings.dll /out:bin\Debug\imageio.dll /doc:bin\Debug\imageio.xml @src/imageio.NET/imageio.NET.Windows.rsp
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe @ConfigurationDeb.rsp /r:bin\Debug\System.FloatingPoint.dll /out:bin\Debug\System.Complex.dll /doc:bin\Debug\System.Complex.xml @src/System.Complex/System.Complex.Windows.rsp
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe @ConfigurationDeb.rsp /out:bin\Debug\PyType.dll /doc:bin\Debug\PyType.xml @src/PyType.NET/PyType.NET.Windows.rsp
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe @ConfigurationRel.rsp /r:bin\Debug\System.FloatingPoint.dll /r:bin\Debug\warnings.dll /out:bin\Debug\torchfile.dll /doc:bin\Debug\torchfile.xml @src/torchfile.NET/torchfile.NET.Windows.rsp
 cmd.exe
--- Implementation/Compile.sh
+++ Implementation/Compile.sh
@@ -5,9 +5,11 @@ csc @ConfigurationRel.rsp /out:bin/Release/warnings.dll /doc:bin/Release/warning
 csc @ConfigurationRel.rsp /r:bin/Release/System.FloatingPoint.dll /r:bin/Release/warnings.dll /out:bin/Release/imageio.dll /doc:bin/Release/imageio.xml @src/imageio.NET/imageio.NET.Unix.rsp
 csc @ConfigurationRel.rsp /r:bin/Release/System.FloatingPoint.dll /out:bin/Release/System.Complex.dll /doc:bin/Release/System.Complex.xml @src/System.Complex/System.Complex.Unix.rsp
 csc @ConfigurationRel.rsp /out:bin/Release/PyType.dll /doc:bin/Release/PyType.xml @src/PyType.NET/PyType.NET.Unix.rsp
+csc @ConfigurationRel.rsp /r:bin/Release/System.FloatingPoint.dll /r:bin/Release/warnings.dll /out:bin/Release/torchfile.dll /doc:bin/Release/torchfile.xml @src/torchfile.NET/torchfile.NET.Unix.rsp
 mkdir bin/Debug
 csc @ConfigurationDeb.rsp /out:bin/Debug/System.FloatingPoint.dll /doc:bin/Debug/System.FloatingPoint.xml @src/System.FloatingPoint/System.FloatingPoint.Unix.rsp
 csc @ConfigurationDeb.rsp /out:bin/Debug/warnings.dll /doc:bin/Debug/warnings.xml @src/warnings.NET/warnings.NET.Unix.rsp
 csc @ConfigurationDeb.rsp /r:bin/Debug/System.FloatingPoint.dll /r:bin/Debug/warnings.dll /out:bin/Debug/imageio.dll /doc:bin/Debug/imageio.xml @src/imageio.NET/imageio.NET.Unix.rsp
 csc @ConfigurationDeb.rsp /r:bin/Debug/System.FloatingPoint.dll /out:bin/Debug/System.Complex.dll /doc:bin/Debug/System.Complex.xml @src/System.Complex/System.Complex.Unix.rsp
 csc @ConfigurationDeb.rsp /out:bin/Debug/PyType.dll /doc:bin/Debug/PyType.xml @src/PyType.NET/PyType.NET.Unix.rsp
+csc @ConfigurationDeb.rsp /r:bin/Debug/System.FloatingPoint.dll /r:bin/Debug/warnings.dll /out:bin/Debug/torchfile.dll /doc:bin/Debug/torchfile.xml @src/torchfile.NET/torchfile.NET.Unix.rsp
